8bbc25633bc1150f60ec79e513a6aa76d04a8a92
MARLOWWW88 <marlow20112026@hotmail.com>
2026-02-09 20:47:50 +0800

    Site updated: 2026-02-09 20:47:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/05/hello-world/","2026/02/08/school/"];function toRandomPost(){
+var posts=["2026/02/05/hello-world/","2026/02/08/school/","2026/02/08/telegraph-Image：搭建你的专属开源图床/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
